a4b25f9a41177f82fca7ab59c436cae225fc7970
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-07-02 12:45:57 +0100

    replace os.path.join with f-strings

--- __init__.py
+++ __init__.py
@@ -6,16 +6,9 @@ sunrises and sunsets of all territories in the BOT. It uses this data to
 demonstrate that the sun has not (yet) set over the BOT.
 """
 
-# Import standard modules ...
-import sys
-
 # Import sub-functions ...
 from .create_db import create_db
 from .create_map import create_map
 from .create_maps import create_maps
 from .create_timeline import create_timeline
 from .run import run
-
-# Ensure that this module is only imported by Python 3.x ...
-if sys.version_info.major != 3:
-    raise Exception("the Python module \"bots\" must only be used with Python 3.x") from None
